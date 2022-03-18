d4a79a00352f6bd207b20c51ad8d72077a09767e
Maylson <maylsonmoraesd@hotmail.com>
2022-03-18 15:03:44 -0300

    navibar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,8 +12,8 @@
         <!--bootstrap-->    
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!--css da aplicação-->
-        <link rel="stylesheet" href="/resources/css/app.css">
-        <script src="/resources/js/app.js"></script>
+        <link rel="stylesheet" href="{{asset('/public/css/app.css')}}">
+        <script src="/public/js/app.js"></script>
     </head>
     <body>
         <header>
