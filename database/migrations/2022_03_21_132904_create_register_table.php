069347d4b180e25aac898561524a86a7439abf89
Maylson <maylsonmoraesd@hotmail.com>
2022-03-21 16:06:47 -0300

    mudanças de migration

--- database/migrations/2022_03_21_132904_create_register_table.php
+++ database/migrations/2022_03_21_132904_create_register_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateRegisterTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('register', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->integer('theamount');
-            $table->text('description');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->integer('fone');
+            $table->string('address');
+            $table->integer('zip');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('register');
     }
 }
