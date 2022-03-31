8a8ff0ef99ff701954a726c47281c11cb87b3bff
Maylson <maylsonmoraesd@hotmail.com>
2022-03-31 10:54:58 -0300

    dashboard recebendo dados do banco de usuários

--- resources/views/programs/dashboard.blade.php
+++ resources/views/programs/dashboard.blade.php
@@ -9,8 +9,28 @@
 </div>
 <div class="col-md-10 offset-md-1 dashboard-programs-container">
     @if(count($programs) > 0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Alunos</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($programs as $program)
+            <tr>
+                <td scropt="row">{{ $loop->index + 1 }}</td>
+                <td><a href="/programs/{{ $program->id }}">{{ $program->name }}</a></td>
+                <td>0</td>
+                <td><a href="#">Editar</a> <a href="#">Deletar</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
     @else
-    <p>Você ainda nao possui programas marciais cadastrados,<a href="/programs/create">criar evento</a></p>
+    <p>Você ainda nao possui programas marciais cadastrados,<a href="/programs/create">Criar Programa Marcial</a></p>
     @endif
 </div>
 @endsection
